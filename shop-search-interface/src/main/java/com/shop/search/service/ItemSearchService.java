a8ea76a4a968aa5c56a453a1a787fdfb333be7bf
Viki Chen <viki@chinadep.com>
2019-01-20 21:10:15 +0800

    1.整合solr

---
+++ shop-search-interface/src/main/java/com/shop/search/service/ItemSearchService.java
@@ -0,0 +1,17 @@
+package com.shop.search.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ItemSearchService {
+
+	
+	/**
+	 * 搜索方法
+	 * @param searchMap
+	 * @return
+	 */
+	public Map search(Map searchMap);
+	
+	
+}
